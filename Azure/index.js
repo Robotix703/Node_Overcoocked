831ca4caa94ea8daade5a0147c6ad55a52ec21ac
Robotix703 <stephane.chatelle@orange.fr>
2022-01-01 12:04:54 +0000

    Azure : Gestion récupération autorisation utilisateur

--- Azure/index.js
+++ Azure/index.js
@@ -1,42 +1,66 @@
-#!/usr/bin/env node
-
-// read in env settings
+/*
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License.
+ */
 require('dotenv').config();
+const express = require("express");
+const msal = require('@azure/msal-node');
 
-const yargs = require('yargs');
+const SERVER_PORT = process.env.PORT || 3000;
+const REDIRECT_URI = "http://localhost:3000/redirect";
 
-const fetch = require('./fetch');
-const auth = require('./auth');
-
-const options = yargs
-    .usage('Usage: --op <operation_name>')
-    .option('op', { alias: 'operation', describe: 'operation name', type: 'string', demandOption: true })
-    .argv;
-
-async function main() {
-    console.log(`You have selected: ${options.op}`);
-
-    switch (yargs.argv['op']) {
-        case 'getUsers':
-
-            try {
-                // here we get an access token
-                const authResponse = await auth.getToken(auth.tokenRequest);
-
-                // call the web API with the access token
-                const users = await fetch.callApi(auth.apiConfig.uri, authResponse.accessToken);
-
-                // display result
-                console.log(users);
-            } catch (error) {
-                console.log(error);
-            }
-
-            break;
-        default:
-            console.log('Select a Graph operation first');
-            break;
-    }
+// Before running the sample, you will need to replace the values in the config, 
+// including the clientSecret
+const config = {
+    auth: {
+        clientId: "58878e16-2e16-4bc3-81b2-10e1398b6a9a",
+        authority: "https://login.microsoftonline.com/common",
+        clientSecret: "_DZ7Q~fo_dJI4TJ4g5v4DtCicGQ9ITho2Y~V-"
+    },
+    system: {
+        loggerOptions: {
+            loggerCallback(loglevel, message, containsPii) {
+                console.log(message);
+            },
+            piiLoggingEnabled: false,
+            logLevel: msal.LogLevel.Verbose,
+        }
+    }
 };
 
-main();
+// Create msal application object
+const pca = new msal.ConfidentialClientApplication(config);
+
+// Create Express App and Routes
+const app = express();
+
+app.get('/', (req, res) => {
+    const authCodeUrlParameters = {
+        scopes: ["user.read"],
+        redirectUri: REDIRECT_URI,
+    };
+
+    // get url to sign user in and consent to scopes needed for application
+    pca.getAuthCodeUrl(authCodeUrlParameters).then((response) => {
+        res.redirect(response);
+    }).catch((error) => console.log(JSON.stringify(error)));
+});
+
+app.get('/redirect', (req, res) => {
+    const tokenRequest = {
+        code: req.query.code,
+        scopes: ["user.read"],
+        redirectUri: REDIRECT_URI,
+    };
+
+    pca.acquireTokenByCode(tokenRequest).then((response) => {
+        console.log("\nResponse: \n:", response);
+        res.sendStatus(200);
+    }).catch((error) => {
+        console.log(error);
+        res.status(500).send(error);
+    });
+});
+
+
+app.listen(SERVER_PORT, () => console.log(`Msal Node Auth Code Sample app listening on port ${SERVER_PORT}!`))
