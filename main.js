b0ea7b8d0407e06252b81556c0d3556d56643fdf
Robotix703 <stephane.chatelle@orange.fr>
2022-01-01 14:28:38 +0000

    Suppression Azure

--- main.js
+++ main.js
@@ -1,12 +1,8 @@
 const SMS = require("./sms/sms");
-const Azure = require("./Azure/main");
 const Todoist = require("./Todoist/main");
-
 
 //let tel = process.env.INDFR + process.env.TEL;
 //SMS.SendSMS([tel], "Hello World !!");
-
-//Azure.GetTasks();
 
 Todoist.getItemsInProjectByName(process.env.TODOPROJECT).then((items) => {
     console.log(items);
