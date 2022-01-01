b6c146c0e40696bf11ab7b356de9f493f12169c1
Robotix703 <stephane.chatelle@orange.fr>
2022-01-01 14:26:39 +0000

    Todoist: Ajout API

--- main.js
+++ main.js
@@ -1,8 +1,17 @@
 const SMS = require("./sms/sms");
 const Azure = require("./Azure/main");
+const Todoist = require("./Todoist/main");
 
 
-let tel = process.env.INDFR + process.env.TEL;
+//let tel = process.env.INDFR + process.env.TEL;
 //SMS.SendSMS([tel], "Hello World !!");
 
-Azure.GetTasks();
+//Azure.GetTasks();
+
+Todoist.getItemsInProjectByName(process.env.TODOPROJECT).then((items) => {
+    console.log(items);
+});
+
+Todoist.addItemsInProjectByName(process.env.TODOPROJECT, "Hello World !").then(() => {
+    
+})
