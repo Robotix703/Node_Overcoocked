72c97c1d1eb084984667f934659589b8bdf22adf
Robotix703 <stephane.chatelle@orange.fr>
2022-06-30 15:43:05 +0200

    Skip module TU

--- unitTests/modules/todoist.test.js
+++ unitTests/modules/todoist.test.js
@@ -3,28 +3,28 @@ const todoist = require("../../build/modules/todoist");
 let taskText = "TEST";
 let taskID;
 
-test('Get project name', async () => {
+test.skip('Get project name', async () => {
     let projectName = await todoist.Todoist.getProjectID("CoursesDev");
     expect(projectName).toBe(2283408337);
 });
 
-test('Get tasks', async () => {
+test.skip('Get tasks', async () => {
     let tasks = await todoist.Todoist.getItemsInProjectByName("CoursesDev");
     expect(tasks.length).toBe(3);
 });
 
-test('Add tasks', async () => {
+test.skip('Add tasks', async () => {
     let task = await todoist.Todoist.addItemsInProjectByName("CoursesDev", taskText);
     taskID = task.id;
     expect(task.created.length).not.toBe(0);
 });
 
-test('Update tasks', async () => {
+test.skip('Update tasks', async () => {
     let task = await todoist.Todoist.updateItem(taskID, taskText + "New");
     expect(task).toBe(true);
 });
 
-test('Delete tasks', async () => {
+test.skip('Delete tasks', async () => {
     let task = await todoist.Todoist.deleteItem(taskID);
     expect(task).toBe(true);
 });
